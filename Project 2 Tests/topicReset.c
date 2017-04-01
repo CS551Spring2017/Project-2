1fd28e255d52df77f267be3ae4535e1fbd561192
Randal Kwok <randalkwok@yahoo.com>
2017-03-31 23:35:54 -0500

    topic reset

---
+++ Project 2 Tests/topicReset.c
@@ -0,0 +1,10 @@
+#include <lib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+	topicReset();
+}
